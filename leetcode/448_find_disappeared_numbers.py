23c8a224668c894f914be25b721c65b0fbca0b63
ob1-cuna <anacletocuna@gmail.com>
2023-12-26 15:39:51 +0200

    Adiicionado final para problema 448

--- leetcode/448_find_disappeared_numbers.py
+++ leetcode/448_find_disappeared_numbers.py
@@ -3,10 +3,8 @@ lista_2 = [1,1]
 
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        lista = []
         nums_sorted = list(set(nums))
-        [lista.append(n) for n in range(1, len(nums) + 1) if n not in nums_sorted]
-        return lista
+        return list(set(range(1, len(nums) + 1)).difference(nums_sorted))
     
 solucao = Solution()
 print(solucao.findDisappearedNumbers(lista))
