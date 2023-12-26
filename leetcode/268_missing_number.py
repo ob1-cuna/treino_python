8c3d0928b4871e7b5f3077485db4fb6730c5b85c
ob1-cuna <anacletocuna@gmail.com>
2023-12-26 14:37:13 +0200

    adicionada solucao de problema 268 do leet code

--- leetcode/268_missing_number.py
+++ leetcode/268_missing_number.py
@@ -1,3 +1,12 @@
+lista = [9,6,4,2,3,5,7,0,1]
+
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        ...
+        nums_sorted = sorted(nums)
+        for n in range(len(nums)+1):
+                if n not in nums_sorted:
+                    return n
+
+solucao = Solution()
+print(solucao.missingNumber(lista))
+            
