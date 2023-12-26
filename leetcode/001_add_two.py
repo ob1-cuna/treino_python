75e73eaa05d99d776e6fcf117fdd3699885c3be0
ob1-cuna <anacletocuna@gmail.com>
2023-12-26 14:00:53 +0200

    Adicionado problemas do leetcode

---
+++ leetcode/001_add_two.py
@@ -0,0 +1,11 @@
+lista = [3,2,4]
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j]) == target:
+                    return [i,j]
+
+solucao = Solution()
+print(solucao.twoSum(lista, 6))
