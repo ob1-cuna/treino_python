2902ffe273b50b3334bf237daac5507f056ed3b6
ob1-cuna <anacletocuna@gmail.com>
2023-12-29 13:17:20 +0200

    updated version of problem 268, using hashmaps instead of arrays, and added new files to .gitignore

--- leetcode/268_missing_number.py
+++ leetcode/268_missing_number.py
@@ -2,10 +2,11 @@ lista = [9,6,4,2,3,5,7,0,1]
 
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        nums_sorted = sorted(nums)
-        for n in range(len(nums)+1):
-                if n not in nums_sorted:
-                    return n
+        numbers_dict = dict(zip(nums, range(len(nums))))
+        for i in range(len(nums)+1):
+            if i not in numbers_dict.keys():
+                return i
+        
 
 solucao = Solution()
 print(solucao.missingNumber(lista))
