c8f4cc9c0afa5621c8a2b20cf3120c8db37b7f0a
ob1-cuna <anacletocuna@gmail.com>
2024-01-06 13:27:54 +0200

    adicionada solucao para problema numero 137 do leetcode

---
+++ leetcode/137_single_number.py
@@ -0,0 +1,27 @@
+# Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+# Example 1:
+# Input: nums = [2,2,3,2]
+# Output: 3
+
+# Example 2:
+# Input: nums = [0,1,0,1,0,1,99]
+# Output: 99
+ 
+
+# Constraints:
+# 1 <= nums.length <= 3 * 104
+# -231 <= nums[i] <= 231 - 1
+# Each element in nums appears exactly three times except for one element which appears once.
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        nums_dict = {}
+        for n in nums:
+            if n in nums_dict:
+                nums_dict[n] = nums_dict[n] + 1
+            else:
+                nums_dict[n] = 1
+        return sorted(nums_dict.items(), key=lambda x:x[1])[0][0]
