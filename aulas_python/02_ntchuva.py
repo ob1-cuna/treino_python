e74772de6e8b5bf6d4d3ef23e6c660af9f6c54c5
ob1-cuna <anacletocuna@gmail.com>
2024-01-09 21:13:58 +0200

    🎮   GAME OVER feito e jogo de tabuleiro concluido.

--- aulas_python/02_ntchuva.py
+++ aulas_python/02_ntchuva.py
@@ -1,9 +1,9 @@
 import random
 
-tabuleiro_player_2 = [[4, 4, 4, 4],
-                      [4, 4, 4, 4]]
+tabuleiro_player_2 = [[0, 0, 0, 0],
+                      [0, 1, 0, 0]]
 
-tabuleiro_player_1 = [[4, 4, 4, 4],
+tabuleiro_player_1 = [[4, 0, 1, 4],
                       [4, 4, 4, 4]]
 
 
@@ -13,8 +13,12 @@ coordenadas_P2 = {"A1": (0,0), "A2": (0,1), "A3": (0,2), "A4": (0,3),
 coordenadas_P1 = {"C1": (0,0), "C2": (0,1), "C3": (0,2), "C4": (0,3),
                   "D1": (1,0), "D2": (1,1), "D3": (1,2), "D4": (1,3)}
 
+pontos_player_2:int = 0
+pontos_player_1:int = 31
+
 quem_joga = 1
-vencedor = None 
+vencedor = None
+
 
 def ver_tabuleiro():
     p2_letras = ["   ", "1", "2", "3", "4", "   "]
@@ -42,7 +46,7 @@ def ver_tabuleiro():
     print("   --------- ")
     for p1_letra in p1_letras:
         print(p1_letra, end=" ")
-    print("")
+    print(f"\n\nP1 = {pontos_player_1}    P2 = {pontos_player_2}\nPedras Capuradas")
 
 
 def obter_coordenadas(coordenada:str, tabuleiro:dict):
@@ -54,6 +58,8 @@ def obter_coordenadas(coordenada:str, tabuleiro:dict):
 def mover_peca(x: int, y: int, tabuleiro:list):
   casas_a_percorrer = tabuleiro[x][y]
   casas_percorridas = 0
+  global pontos_player_1, pontos_player_2
+
 
   while casas_a_percorrer > casas_percorridas:
         for _ in range(casas_a_percorrer + 1):
@@ -98,11 +104,14 @@ def mover_peca(x: int, y: int, tabuleiro:list):
                     elif tabuleiro[x][y] == 1: # CAPTURA DE PEÇA
                         if tabuleiro == tabuleiro_player_1 and x == 0:
                             if tabuleiro_player_2[x+1][y] > 0:
-                                print(f"\n Capuradas {tabuleiro_player_2[x+1][y]} peças do Jogador 2")
+                                pontos_player_1 = pontos_player_1 + tabuleiro_player_2[x+1][y]
+                                print(f"\nCapuradas {tabuleiro_player_2[x+1][y]} peças do Jogador 2")
                                 tabuleiro_player_2[x+1][y] = 0
+                                
                         elif tabuleiro == tabuleiro_player_2 and x == 1:
                             if tabuleiro_player_1[x-1][y] > 0:
-                                print(f"\n Capuradas {tabuleiro_player_1[x-1][y]} peças do Jogador 1")
+                                pontos_player_2 = pontos_player_2 + tabuleiro_player_1[x-1][y]
+                                print(f"\nCapuradas {tabuleiro_player_1[x-1][y]} peças do Jogador 1")
                                 tabuleiro_player_1[x-1][y] = 0
           x = next_x
           y = next_y    
@@ -133,18 +142,25 @@ def jogar(tabuleiro_jogador):
             print("Coordenada desconhecida. Tente novamente.")
 
 def main():
-    global quem_joga, vencedor
+    global quem_joga, vencedor, pontos_player_1, pontos_player_2
     ver_tabuleiro()
-    print()
     while vencedor is None:
         if quem_joga == 1:
             print("\nJogador 1")
             jogar(tabuleiro_player_1)
-            quem_joga = 2
+            if pontos_player_1 > 31:
+                vencedor = pontos_player_1
+                print("GAME OVER")
+            else:
+                quem_joga = 2
         else:
             print("\nJogador 2")
             jogar(tabuleiro_player_2)
-            quem_joga = 1
+            if pontos_player_2 >= 31:
+                vencedor = pontos_player_2
+                print("GAME OVER")
+            else:
+                quem_joga = 1
 
 if __name__ == "__main__":
     main()
