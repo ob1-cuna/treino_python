2a7f2444b712c31f015391410d0ab8bb4ea7f543
ob1-cuna <anacletocuna@gmail.com>
2024-01-10 13:56:13 +0200

    🪖 📝  adicionada regra de remoção das pedras externas.

--- aulas_python/02_ntchuva.py
+++ aulas_python/02_ntchuva.py
@@ -1,49 +1,49 @@
 import random
 
-tabuleiro_player_2 = [[4, 4, 4, 4],
-                      [4, 4, 4, 4]]
+tabuleiro_player_2 = [[2, 2, 2, 2, 2, 2],
+                      [2, 2, 2, 2, 2, 2]]
 
-tabuleiro_player_1 = [[4, 4, 4, 4],
-                      [4, 4, 4, 4]]
+tabuleiro_player_1 = [[2, 2, 2, 2, 2, 2],
+                      [2, 2, 2, 2, 2, 2]]
 
+coordenadas_P2 = {"A1": (0,0), "A2": (0,1), "A3": (0,2), "A4": (0,3), "A5": (0,4), "A6": (0,5),
+                  "B1": (1,0), "B2": (1,1), "B3": (1,2), "B4": (1,3), "B5": (1,4), "B6": (1,5)}
 
-coordenadas_P2 = {"A1": (0,0), "A2": (0,1), "A3": (0,2), "A4": (0,3),
-                  "B1": (1,0), "B2": (1,1), "B3": (1,2), "B4": (1,3)}
-
-coordenadas_P1 = {"C1": (0,0), "C2": (0,1), "C3": (0,2), "C4": (0,3),
-                  "D1": (1,0), "D2": (1,1), "D3": (1,2), "D4": (1,3)}
+coordenadas_P1 = {"C1": (0,0), "C2": (0,1), "C3": (0,2), "C4": (0,3), "C5": (0,4), "C6": (0,5),
+                  "D1": (1,0), "D2": (1,1), "D3": (1,2), "D4": (1,3), "D5": (1,4), "D6": (1,5)}
 
 pontos_player_2:int = 0
 pontos_player_1:int = 0
 
+total_pedras_p1, total_pedras_p2 = sum(map(sum,tabuleiro_player_1)), sum(map(sum,tabuleiro_player_2))
+
 quem_joga = 1
 vencedor = None
 
-
 def ver_tabuleiro():
-    p2_letras = ["   ", "1", "2", "3", "4", "   "]
-    p1_letras = ["   ", "1", "2", "3", "4", "   "]
+    p2_letras = ["   ", "1", "2", "3", "4", "5", "6","   "]
+    p1_letras = ["   ", "1", "2", "3", "4", "5", "6","   "]
     index_letra = ["A", "B", "C", "D"]
     
     for p2_letra in p2_letras:
         print(p2_letra, end=" ")
-    print("\n   ---------  ")
+    print("\n   ------------  ")
     for index, linha in enumerate(tabuleiro_player_2):
         print(index_letra[index], end=" | ")
         for index_col, coluna in enumerate(linha):
             print(coluna, end=" ")
-            if index_col == 3:
+            if index_col == 5:
                 print(f"| {index_letra[index]}", end="")
         print("")
-    print("   --------- ")
+    print("   ------------ ")
     for index, linha in enumerate(tabuleiro_player_1):
         print(index_letra[index+2], end=" | ")
         for index_col, coluna in enumerate(linha):
             print(coluna, end=" ")
-            if index_col == 3:
+            if index_col == 5:
                 print(f"| {index_letra[index+2]}", end="")
         print("")
-    print("   --------- ")
+    print("   ------------ ")
     for p1_letra in p1_letras:
         print(p1_letra, end=" ")
     print(f"\n\nP1 = {pontos_player_1}    P2 = {pontos_player_2}\nPedras Capuradas")
@@ -56,17 +56,20 @@ def obter_coordenadas(coordenada:str, tabuleiro:dict):
         return "Posição desconhecida."
 
 def mover_peca(x: int, y: int, tabuleiro:list):
-  casas_a_percorrer = tabuleiro[x][y]
-  casas_percorridas = 0
-  global pontos_player_1, pontos_player_2
+  casas_a_percorrer = tabuleiro[x][y] # define o numero de casas que irá percorrer
+  casas_percorridas = 0 # helper
+  global pontos_player_1, pontos_player_2 # número actual de pontos dos jogadores.
 
 
   while casas_a_percorrer > casas_percorridas:
-        for _ in range(casas_a_percorrer + 1):
-          valor = tabuleiro[x][y]
+        for _ in range(casas_a_percorrer + 1): # +1 para adicionar mais um movimento, pois inicia a contagem a partir da remoção da pedra.
+          valor = tabuleiro[x][y] # helper
+          
+          # Verifica a posição e e diz ao programa que posição é a proxima a mover
+
           if x == 0 and y == 0:
-              next_x = 1
-              next_y = 0
+            next_x = 1
+            next_y = 0
           elif x == 1 and y == 0:
             next_x = 1
             next_y = 1
@@ -77,6 +80,18 @@ def mover_peca(x: int, y: int, tabuleiro:list):
             next_x = 1
             next_y = 3
           elif x == 1 and y == 3:
+            next_x = 1
+            next_y = 4
+          elif x == 1 and y == 4:
+            next_x = 1
+            next_y = 5
+          elif x == 1 and y == 5:
+            next_x = 0
+            next_y = 5        
+          elif x == 0 and y == 5:
+            next_x = 0
+            next_y = 4
+          elif x == 0 and y == 4:
             next_x = 0
             next_y = 3
           elif x == 0 and y == 3:
@@ -89,30 +104,52 @@ def mover_peca(x: int, y: int, tabuleiro:list):
             next_x = 0
             next_y = 0
           
+          # Movimento da primeira casa, que remove as pedras da casa e inicia a distribuição na proxima casa no sentido anti-horário.
           if casas_percorridas == 0:
             casas_percorridas += 1
             tabuleiro[x][y] = 0
             print(f"\n{casas_percorridas}. pos(x={x}, y={y}), {valor} -> {tabuleiro[x][y]}")
           else:
+              # Caso não seja a primeira casa continua a distribuição no sentido anti-horário.
               casas_percorridas += 1
               tabuleiro[x][y] += 1
-              print(f"{casas_percorridas}. pos(x={x}, y={y}), {valor} -> {tabuleiro[x][y]}")
-              if (casas_percorridas-1 == casas_a_percorrer):
-                    if tabuleiro[x][y] > 1:
-                        print(f"\nPeças a mover: {tabuleiro[x][y]}")
-                        mover_peca(x, y, tabuleiro)
+              print(f"{casas_percorridas}. pos(x={x}, y={y}), {valor} -> {tabuleiro[x][y]}") # Imprime o relatório da posição actual, pedras existentes anteriorimente e novo número de pedras.
+              if (casas_percorridas-1 == casas_a_percorrer): # Verifica se está no ultimo movimento
+                    if tabuleiro[x][y] > 1: # se a casa onde foi feito o ultimo movimento número de peças for maior que 1
+                        print(f"\nPeças a mover: {tabuleiro[x][y]}") # Informa o número de peças que irá mover
+                        mover_peca(x, y, tabuleiro) # Aplica novamente a função usando os parametros da posição actual.
+                    
                     elif tabuleiro[x][y] == 1: # CAPTURA DE PEÇA
-                        if tabuleiro == tabuleiro_player_1 and x == 0:
-                            if tabuleiro_player_2[x+1][y] > 0:
-                                pontos_player_1 = pontos_player_1 + tabuleiro_player_2[x+1][y]
-                                print(f"\nCapuradas {tabuleiro_player_2[x+1][y]} peças do Jogador 2")
-                                tabuleiro_player_2[x+1][y] = 0
+                        if tabuleiro == tabuleiro_player_1 and x == 0: # verifica se está na posição interna
+                            if tabuleiro_player_2[x+1][y] > 0: # verifica se o numero de pedras do adversário na linha imediatamente a seguir do jogador 1 tem ou não peças
+                                pedras_externas = 0 # helper
+                                pedras_internas = tabuleiro_player_2[x+1][y] # helper
                                 
+                                tabuleiro_player_2[x+1][y] = 0 # recolhe as pedras internas do adversário
+                                pontos_player_1 = pontos_player_1 + pedras_internas # converte o número de pedras removidas do jogador 2 em pontos
+                                
+                                if tabuleiro_player_2[x][y] > 0: # verfica se a coluna externa tem ou não pedras
+                                  pedras_externas = tabuleiro_player_2[x][y] # helper
+                                  tabuleiro_player_2[x][y] = 0 # recolhe as pedras externas
+                                  pontos_player_1 = pontos_player_1 + pedras_externas # converte o número de pedras removidas do jogador 2 em pontos
+                            
+                                print(f"\nCapuradas {pedras_internas + pedras_externas} peças do Jogador 1") # informa aos jogadores quantas peças foram removidas
+                        
                         elif tabuleiro == tabuleiro_player_2 and x == 1:
                             if tabuleiro_player_1[x-1][y] > 0:
-                                pontos_player_2 = pontos_player_2 + tabuleiro_player_1[x-1][y]
-                                print(f"\nCapuradas {tabuleiro_player_1[x-1][y]} peças do Jogador 1")
+                                pedras_externas = 0
+                                pedras_internas = tabuleiro_player_1[x-1][y]
+
                                 tabuleiro_player_1[x-1][y] = 0
+                                pontos_player_2 = pontos_player_2 + pedras_internas
+
+                                if tabuleiro_player_1[x][y] > 0:
+                                   pedras_externas = tabuleiro_player_1[x][y]
+                                   tabuleiro_player_1[x][y] = 0
+                                   pontos_player_2 = pontos_player_2 + pedras_internas
+                                
+                                print(f"\nCapuradas {pedras_internas + pedras_externas} peças do Jogador 1")
+
           x = next_x
           y = next_y    
 
@@ -167,7 +204,7 @@ def main():
         if quem_joga == 1:
             print("\nJogador 1")
             jogar(tabuleiro_player_1)
-            if pontos_player_1 > 31:
+            if pontos_player_1 > total_pedras_p2 - 1:
                 vencedor = pontos_player_1
                 print("\n   GAME OVER  \nJOGADOR 1 VENCEU\n")
             else:
@@ -175,7 +212,7 @@ def main():
         else:
             print("\nJogador 2")
             jogar(tabuleiro_player_2)
-            if pontos_player_2 >= 31:
+            if pontos_player_2 > total_pedras_p1 - 1:
                 vencedor = pontos_player_2
                 print("\n   GAME OVER  \nJOGADOR 2 VENCEU\n")
             else:
